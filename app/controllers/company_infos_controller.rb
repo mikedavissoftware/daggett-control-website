ed94dd019b12a21700b6933f3fcaa31cf3ab0096
Mike Davis <micdavis93@gmail.com>
2023-03-19 14:00:45 -0600

    added info to home page

--- app/controllers/company_infos_controller.rb
+++ app/controllers/company_infos_controller.rb
@@ -1,25 +1,19 @@
 class CompanyInfosController < ApplicationController
-  
-  def index
-
-  end
 
   def show
-
-  end
-
-  def create
-
+    company_info = CompanyInfo.find(params[:id])
+    render json: company_info
   end
 
   def update
-
-  end
-
-  def destroy
-
+    company_info = CompanyInfo.find(params[:id])
+    company_info.update!(company_info_params)
   end
 
   private
   
+  def company_info_params
+    params.permit(:name, :byline, :bio)
+  end
+
 end
--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,14 +1,24 @@
-
-
+import { useState, useEffect } from "react"
 
 
 export default function HomePage() {
+  const [companyInfo, setCompanyInfo] = useState({})
 
-
+  useEffect(() => {
+    fetch("/api/company_infos/1")
+    .then(r => r.json())
+    .then(companyInfoData => {
+      setCompanyInfo(companyInfoData)
+    })
+  }, [])
+  
+  const {name, byline, bio} = companyInfo
 
   return (
     <div>
-      <h2 className="text-3xl">This is the HomePage component</h2>
+      <h2 className="text-3xl"><em>Company Name: </em>{name}</h2>
+      <h3 className="text-xl my-2"><em>Byline: </em>{byline}</h3>
+      <p className="my-4"><em>Bio: </em>{bio}</p>
     </div>
   )
 }
