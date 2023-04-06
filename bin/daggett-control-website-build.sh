6beaa803baed6b1935eeefc18af1b40a76a634fd
Michael Davis <micdavis93@gmail.com>
2023-04-06 14:32:42 -0600

    deployment bin files created

---
+++ bin/daggett-control-website-build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+bundle install
+bundle exec rake assets:precompile # These lines would be commented out if we had an API only app
+bundle exec rake assets:clean
+bundle exec rake db:migrate 
+bundle exec rake db:seed
