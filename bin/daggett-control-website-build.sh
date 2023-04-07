904398b6dbbbaa9b95e9d02a337286b2f36045c7
Michael Davis <micdavis93@gmail.com>
2023-04-06 22:59:56 -0600

    new migration and database, revised database.yml

--- bin/daggett-control-website-build.sh
+++ bin/daggett-control-website-build.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 bundle install
-# bundle exec rake assets:precompile # These lines would be commented out if we had an API only app
-# bundle exec rake assets:clean
+bundle exec rake assets:precompile # These lines would be commented out if we had an API only app
+bundle exec rake assets:clean
 bundle exec rake db:migrate 
 bundle exec rake db:seed
