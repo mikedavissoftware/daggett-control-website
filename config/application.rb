bedc44b8b0faa99646e07a0fcd168d04b30ec909
Michael Davis <micdavis93@gmail.com>
2023-04-06 22:00:18 -0600

    corrected contact_forms_controller

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,6 @@ module DaggettControlWebsite
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    config.autoloader = :classic
-
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
