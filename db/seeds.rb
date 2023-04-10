ca4b56b1ad77abba48103caf730637d8f19df636
Mike Davis <micdavis93@gmail.com>
2023-04-10 09:04:24 -0600

    restored seeds file to original state, commented out rake db:seed in sh file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,30 +2,30 @@ puts "Now seeding..."
 
 # Create Team Members
 puts "Creating Users..."
-# User.create(
-#   username: "ron",
-#   password: "123",
-#   name: "Ron Daggett",
-#   role: "Owner and Operator",
-#   bio: "Ron Daggett has decades of experience in industrial controls, and has been an independent industrial control distributor. Ron values customers highly, going above and beyond in providing expertise & guidance to clients.",
-#   website: ""
-# )
-# User.create(
-#   username: "stephanie",
-#   password: "123",
-#   name: "Stephanie Helfer",
-#   role: "Administrator",
-#   bio: "",
-#   website: ""
-# )
-# User.create(
-#   username: "mike",
-#   password: "123",
-#   name: "Mike Davis",
-#   role: "Web Developer & Tech Consultant",
-#   bio: "Mike is a web developer, multimedia producer, and ethical entrepreneur. With many years of experience in technical project management & event production, he brings a tech savviness to Daggett Control that ensures smooth, efficient operation.",
-#   website: "http://mikedavissoftware.com"
-# )
+User.create(
+  username: "ron",
+  password: "123",
+  name: "Ron Daggett",
+  role: "Owner and Operator",
+  bio: "Ron Daggett has decades of experience in industrial controls, and has been an independent industrial control distributor. Ron values customers highly, going above and beyond in providing expertise & guidance to clients.",
+  website: ""
+)
+User.create(
+  username: "stephanie",
+  password: "123",
+  name: "Stephanie Helfer",
+  role: "Administrator",
+  bio: "",
+  website: ""
+)
+User.create(
+  username: "mike",
+  password: "123",
+  name: "Mike Davis",
+  role: "Web Developer & Tech Consultant",
+  bio: "Mike is a web developer, multimedia producer, and ethical entrepreneur. With many years of experience in technical project management & event production, he brings a tech savviness to Daggett Control that ensures smooth, efficient operation.",
+  website: "http://mikedavissoftware.com"
+)
 
 # Create Product Lines
 puts "Creating Product Lines..."
