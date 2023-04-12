a2cba8e023768a7d9cc977a6e687a5a09eca515b
Mike Davis <micdavis93@gmail.com>
2023-04-12 15:16:16 -0600

    changed default route in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,8 +92,8 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
-  config.action_mailer_default_url_options = { host: 'https://daggett-control-website.onrender.com' }
-  Rails.application.routes.default_url_options[:host] = 'https://daggett-control-website.onrender.com'
+  config.action_mailer_default_url_options = { host: 'https://daggett-control.onrender.com' }
+  Rails.application.routes.default_url_options[:host] = 'https://daggett-control.onrender.com'
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
