6f0e66b01821bb9e179cf864d84745109f868564
Mike Davis <micdavis93@gmail.com>
2023-04-12 13:53:39 -0600

    setup for blueprint

---
+++ bin/render-build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+bundle install
+bundle exec rake assets:precompile # These lines would be commented out if we had an API only app
+bundle exec rake assets:clean
+rm -rf public
+npm install --prefix client && npm run build --prefix client
+bundle exec rake db:migrate
+bundle exec rake db:seed
+cp -a client/build/. public/
