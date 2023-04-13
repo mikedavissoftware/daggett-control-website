ad6918960437abfd330fc18d2c9e51b6dcd3ea14
Mike Davis <micdavis93@gmail.com>
2023-04-12 21:51:20 -0600

    restored production.rb completely

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,6 +97,7 @@ Rails.application.configure do
 
 
   config.action_mailer_default_url_options = { host: 'https://daggett-control.onrender.com' }
+  Rails.application.routes.default_url_options[:host] = 'https://daggett-control.onrender.com'
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
