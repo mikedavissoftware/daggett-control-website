f05ee5e19b57b911c49c4e8818944a52d099b644
Michael Davis <micdavis93@gmail.com>
2023-05-01 08:41:40 -0600

    transfer back to desktop

--- client/vite.config.js
+++ client/vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig(({ mode }) => {
           target: apiUrl,
           changeOrigin: true,
           secure: false,
-          rewrite: path => path.replace(/^\/api/, apiUrl),
+          rewrite: path => path.replace(/^\/api/, '/'),
         }
       }
     },
