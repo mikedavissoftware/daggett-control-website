d74ca25d66dedd5d96a504835f3b6a8ee0810afc
Mike Davis <micdavis93@gmail.com>
2023-05-11 08:36:37 -0600

    fixed css on testimonials

--- client/src/components/TestimonialCard.jsx
+++ client/src/components/TestimonialCard.jsx
@@ -6,19 +6,19 @@ export default function TestimonialCard({ testimonial }) {
   const { name, content, website, image, image_as_thumbnail, product_line } = testimonial
 
   return (
-    <div className="card sm:card-side bg-base-100 shadow-xl m-5 items-center">
-      <figure className="w-fit bg-slate-600 sm:max-w-xs">
+    <div className="card sm:card-side bg-base-100 shadow-xl m-5 p-3 sm:p-0 flex items-center">
+      <figure className="w-full max-w-xs">
         {(image) ? (
           <a href={image} target="_blank" className="flex">
-            <img src={image_as_thumbnail} alt={name} className="w-48 block my-5 sm:m-auto"/>
+            <img src={image_as_thumbnail} alt={name} className="rounded-xl sm:rounded-r-none"/>
           </a>
         ) : (
-          <a href={AvatarPlaceholder} target="_blank" className="flex">
-            <img src={AvatarPlaceholder} alt="There is no image." className="w-48 block my-5 sm:m-auto"/>
+          <a href={AvatarPlaceholder} target="_blank" className="">
+            <img src={AvatarPlaceholder} alt="There is no image." className="rounded-xl sm:rounded-r-none"/>
           </a>
         )}
       </figure>
-      <div className="card-body lg:text-left">
+      <div className="card-body sm:text-left">
         <h2 className="font-bold text-xl">{name}</h2>
         <h3 className="italic text-lg">{product_line.company}</h3>
         <p>{content}</p>
