467a009023ac2274eb32c92852736a0c534dc5b3
Mike Davis <micdavis93@gmail.com>
2023-05-22 16:00:39 -0600

    added domain to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -11,6 +11,16 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
+    origins 'https://daggettcontrol.com'
+    resource '/contact_forms',
+      headers: :any, 
+      methods: [:post]
+    resource '*', 
+      headers: :any, 
+      methods: [:get]
+  end
+
+  allow do
     origins 'http://localhost:5173'
     resource '/contact_forms',
       headers: :any, 
