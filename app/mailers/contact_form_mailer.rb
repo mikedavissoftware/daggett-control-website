734050c035fcf82f26badac71ce534a59fbc185a
Mike Davis <micdavis93@DESKTOP-4TSGNL8.>
2024-08-23 15:22:25 -0600

    changed contact submission email to stephanie

--- app/mailers/contact_form_mailer.rb
+++ app/mailers/contact_form_mailer.rb
@@ -3,7 +3,7 @@ class ContactFormMailer < ApplicationMailer
   def new_contact_form_email
     @contact_form = params[:contact_form]
 
-    mail(to: "ron@daggettcontrol.com", subject: "New Contact Form Submission")
+    mail(to: "stephanie@daggettcontrol.com", subject: "New Contact Form Submission")
   end
 
 end
